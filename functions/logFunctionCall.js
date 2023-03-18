d42169c76671ea15a51d51e14c0a6c252997c0be
David <dadch1404@gmail.com>
2023-03-18 09:15:39 +0000

    fix: phasing out logs, now its supabase lets go

--- functions/logFunctionCall.js
+++ functions/logFunctionCall.js
@@ -1,12 +1,14 @@
-const fs = require('fs');
+// const fs = require('fs');
 
 function logFunctionCall(message, fn) {
-  const currentDate = new Date(); // Don't define this outside the function, it will be the same date for all the calls
+  // const currentDate = new Date(); // Don't define this outside the function, it will be the same date for all the calls
 
   return function(...args) {
     message.react('✅');
+		/*
     const logMessage = `[${currentDate.toLocaleString()}] ${fn.name}() called by ${message.from}: ${message.body}`;
     fs.appendFileSync('logs.txt', logMessage + '\n');
+		*/
     return fn(...args);
   }
 }
