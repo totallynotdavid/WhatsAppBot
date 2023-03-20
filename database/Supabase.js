8ba22170a972ced68c31c14ddca11b5708ca094a
David <dadch1404@gmail.com>
2023-03-20 01:18:52 +0000

    fix: lint

--- database/Supabase.js
+++ database/Supabase.js
@@ -2,8 +2,8 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const { createClient } = require('@supabase/supabase-js');
 
-SUPABASE_BASE_URL = process.env.SUPABASE_BASE_URL;
-SUPABASE_API_KEY = process.env.SUPABASE_API_KEY;
+const SUPABASE_BASE_URL = process.env.SUPABASE_BASE_URL;
+const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY;
 
 const supabase = createClient(SUPABASE_BASE_URL, SUPABASE_API_KEY);
 
--- database/connectToDatabase.js
+++ database/connectToDatabase.js
@@ -21,23 +21,23 @@ async function fetchMessages() {
 */
 
 async function insertMessage(phoneNumber, message, group) {
-    let { data: users, error } = await supabase
-        .from('users') // this is the table name
-        .insert({ user: phoneNumber, message: message, group: group }); // this is the column name
+	let { data: /*users,*/ error } = await supabase
+		.from('users') // this is the table name
+		.insert({ user: phoneNumber, message: message, group: group }); // this is the column name
 
-        if (error) {
-            console.log('Could not insert message', error);
-        }
-        /*
-        // Whys is users always null? It doesn't matter if RLS is enabled or not
-        // I'm not sure if this is a bug or if I'm doing something wrong
-        // .from('users') works fine in the fetchMessages() function
-        if (users) {
-            console.log('Message inserted successfully');
-        }
-        */
+		if (error) {
+				console.log('Could not insert message', error);
+		}
+		/*
+		// Whys is users always null? It doesn't matter if RLS is enabled or not
+		// I'm not sure if this is a bug or if I'm doing something wrong
+		// .from('users') works fine in the fetchMessages() function
+		if (users) {
+				console.log('Message inserted successfully');
+		}
+		*/
 }
 
 module.exports = {
-    insertMessage
+    insertMessage,
 };
