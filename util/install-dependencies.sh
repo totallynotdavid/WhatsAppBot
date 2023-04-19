38fdef8118c2b20b86a5759e771d4bf3aa7a63e2
David <dadch1404@gmail.com>
2023-04-19 23:00:49 +0000

    improve codespaces starter

--- util/install-dependencies.sh
+++ util/install-dependencies.sh
@@ -16,8 +16,8 @@ fi
 # Install ffmpeg
 if ! command -v ffmpeg &> /dev/null; then
   echo "FFmpeg is not installed. Installing..."
-  wget -q --show-progress https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
-  wget -q --show-progress https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz.md5
+  wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+  wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz.md5
   md5sum -c ffmpeg-git-amd64-static.tar.xz.md5 > /dev/null
   tar xvf ffmpeg-git-amd64-static.tar.xz -C /tmp > /dev/null
   FFMPEG_DIR=$(find /tmp -type d -name "ffmpeg-git-*")
