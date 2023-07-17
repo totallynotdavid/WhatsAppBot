86d3b37b3cc998e2007997a0ab451a33c5534fb1
David Duran <dadch1404@gmail.com>
2023-07-17 14:09:10 -0500

    feat: :sparkles: Added multiple voices support

--- commands/amazon.js
+++ commands/amazon.js
@@ -1,15 +1,35 @@
 const { PollyClient, SynthesizeSpeechCommand } = require("@aws-sdk/client-polly");
 const fs = require("fs");
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 
 const REGION = "us-east-1";
 const client = new PollyClient({ region: REGION });
 
-async function synthesizeSpeech(text, songId) {
+const voiceOptions = {
+  'Ricardo': 'standard',
+  'Conchita': 'standard',
+  'Lucia': 'neural',
+  'Enrique': 'standard',
+  'Sergio': 'neural',
+  'Mia': 'neural',
+  'Andres': 'neural',
+  'Lupe': 'neural',
+  'Penelope': 'neural',
+  'Miguel': 'standard'
+};
+
+function getRandomVoice() {
+  const voices = Object.keys(voiceOptions);
+  return voices[Math.floor(Math.random() * voices.length)];
+}
+
+async function synthesizeSpeech(text, songId, voiceId) {
   const params = {
     Text: text,
     OutputFormat: 'mp3',
-    VoiceId: 'Ricardo',
+    VoiceId: voiceId,
+		Engine: voiceOptions[voiceId]
   };
 
   try {
@@ -22,8 +42,7 @@ async function synthesizeSpeech(text, songId) {
       return filePath;
     }
   } catch (err) {
-    console.error(err);
-    throw err;
+    throw new Error(`Failed to synthesize speech: ${err.message}`);
   }
 }
 
@@ -33,6 +52,7 @@ async function sendReply(message, replyText, robotEmoji) {
 
 async function handleTextToAudio(stringifyMessage, message, MessageMedia, client, robotEmoji) {
   let textToSpeak = '';
+  let voiceId = getRandomVoice();
 
   if (stringifyMessage.length <= 1) {
     if (message.hasQuotedMsg) {
@@ -41,24 +61,38 @@ async function handleTextToAudio(stringifyMessage, message, MessageMedia, client
       return sendReply(message, 'Lo siento, no puedo leer tu mente. Adjunta el texto que quieres que diga.', robotEmoji);
     }
   } else {
-    textToSpeak = stringifyMessage.slice(1).join(' ');
+    // Check if second word in message is a valid voice
+    if (stringifyMessage[1].startsWith("-")) {
+			let possibleVoiceId = stringifyMessage[1].slice(1).charAt(0).toUpperCase() + stringifyMessage[1].slice(2).toLowerCase();
+			
+			if (voiceOptions.hasOwnProperty(possibleVoiceId)) {
+				voiceId = possibleVoiceId;
+			} else {
+				sendReply(message, 'Voz inválida. Utiliza <!help say> para ver las voces disponibles. Usaremos una voz random.', robotEmoji);
+			}
+		
+			textToSpeak = stringifyMessage.slice(2, stringifyMessage.length).join(' ');  // start speaking from third word onwards
+		} else {
+			sendReply(message, 'Ahora puedes escoger entre varias voces. Utiliza <!help say> para ver las voces disponibles. Usaremos una voz random.', robotEmoji);
+			textToSpeak = stringifyMessage.slice(1).join(' ');  // If no voice is specified, start speaking from second word onwards
+		}
   }
 
   if (textToSpeak.length > 1000) {
     return sendReply(message, 'Lo siento, el texto es demasiado largo. Por favor, limita tu mensaje a 1000 caracteres.', robotEmoji);
   }
 
-  const songId = Math.floor(Math.random() * 1000000);
+  const songId = uuidv4();
   let filePath;
 
   try {
-    filePath = await synthesizeSpeech(textToSpeak, songId)
+    filePath = await synthesizeSpeech(textToSpeak, songId, voiceId);
     const media = MessageMedia.fromFilePath(filePath);
     await client.sendMessage(message.id.remote, media, { sendAudioAsVoice: true });
   } catch (err) {
-    console.error(err);
+    throw new Error(`Failed to handle text to audio: ${err.message}`);
   } finally {
-    if(filePath) {
+    if (filePath) {
       await fs.promises.unlink(filePath);
     }
   }
