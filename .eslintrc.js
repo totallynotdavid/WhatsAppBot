8f0df8eee6706885f3ec869f5c6fcac9c54ecb38
David Duran <dadch1404@gmail.com>
2024-03-21 15:30:52 -0500

    Update ESLint and Prettier configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,59 +1,38 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true,
-        'node': true
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    'extends': `eslint:recommended`,
-    plugins: [
-        `@stylistic/js`
-    ],
-    'overrides': [
+    extends: `eslint:recommended`,
+    plugins: [`@stylistic/js`],
+    overrides: [
         {
-            'env': {
-                'node': true
+            env: {
+                node: true,
             },
-            'files': [
-                `.eslintrc.{js,cjs}`
-            ],
-            'parserOptions': {
-                'sourceType': `script`
-            }
-        }
+            files: [`.eslintrc.{js,cjs}`],
+            parserOptions: {
+                sourceType: `script`,
+            },
+        },
     ],
-    'parserOptions': {
-        'ecmaVersion': `latest`,
-        'sourceType': `module`
+    parserOptions: {
+        ecmaVersion: `latest`,
+        sourceType: `module`,
     },
-    'rules': {
-        'indent': [
-            `error`,
-            4
-        ],
-        'linebreak-style': [
-            `error`,
-            `unix`
-        ],
-        'quotes': [
-            `error`,
-            `backtick`
-        ],
-        'semi': [
-            `error`,
-            `always`
-        ],
-        'multiline-ternary': [
-            `error`,
-            `always-multiline`
-        ],
-        'no-multi-spaces': [
-            `error`
-        ],
-        'function-paren-newline': [
+    rules: {
+        "indent": [`error`, 4],
+        "linebreak-style": [`error`, `unix`],
+        "quotes": [`error`, `backtick`],
+        "semi": [`error`, `always`],
+        "multiline-ternary": [`error`, `always-multiline`],
+        "no-multi-spaces": [`error`],
+        "function-paren-newline": [
             `error`,
             {
-                'minItems': 3
-            }
-        ]
-    }
+                minItems: 3,
+            },
+        ],
+    },
 };
