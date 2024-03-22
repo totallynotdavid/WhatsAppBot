af41172274c7d9b7e908dbeb3adcc0ea2f075e20
David Duran <dadch1404@gmail.com>
2024-03-22 00:21:01 -0500

    feat #15: Add fetch-lyrics package and update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,49 +1,4 @@
-const path = require(`path`);
-require(`dotenv`).config({ path: path.resolve(__dirname, `../.env`) });
-
-const fetch = require(`node-fetch`);
-
-async function fetchSongLyrics(message, robotEmoji) {
-    try {
-        const apiKey = process.env.MUSIXMATCH_API_KEY;
-        let url = `https://api.musixmatch.com/ws/1.1/track.search?format=json&callback=callback&q_track_artist=${message}&f_has_lyrics=1&s_track_rating=desc&apikey=${apiKey}`;
-
-        let response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        let data = await response.json();
-        let trackList = data.message.body.track_list;
-
-        if (!trackList || trackList.length === 0) {
-            url = `https://api.musixmatch.com/ws/1.1/track.search?format=json&callback=callback&q_lyrics=${message}&f_has_lyrics=1&s_track_rating=desc&apikey=${apiKey}`;
-            response = await fetch(url);
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            data = await response.json();
-            trackList = data.message.body.track_list;
-        }
-
-        if (trackList && trackList.length > 0) {
-            const trackId = trackList[0].track.track_id;
-            const lyricsResponse = await fetch(
-                `https://api.musixmatch.com/ws/1.1/track.lyrics.get?format=json&callback=callback&track_id=${trackId}&apikey=${apiKey}`
-            );
-            if (!lyricsResponse.ok) {
-                throw new Error(`HTTP error! status: ${lyricsResponse.status}`);
-            }
-            const lyricsData = await lyricsResponse.json();
-            const lyrics = lyricsData.message.body.lyrics.lyrics_body;
-            return lyrics;
-        }
-
-        return `${robotEmoji} Letras no disponibles para esta canción. ¿Quizá no la escribiste bien?`;
-    } catch (error) {
-        console.error(`Could not fetch lyrics: ${error}`);
-        return `${robotEmoji} Letras no disponibles para esta canción. ¿Puedes intentarlo de nuevo?`;
-    }
-}
+const { getLyrics } = require(`fetch-lyrics`);
 
 async function handleSongLyricsRequest(stringifyMessage, message, robotEmoji) {
     if (stringifyMessage.length === 1) {
@@ -52,11 +7,21 @@ async function handleSongLyricsRequest(stringifyMessage, message, robotEmoji) {
         );
     } else {
         const songName = stringifyMessage.slice(1).join(` `);
-        const songLyrics = await fetchSongLyrics(songName, robotEmoji);
-        if (songLyrics) {
-            message.reply(songLyrics);
-        } else {
-            message.reply(`${robotEmoji} No encontré la letra de esa canción.`);
+        try {
+            const songLyrics = await getLyrics(songName);
+            if (songLyrics && songLyrics.lyrics) {
+                const replyMessage = `${robotEmoji} Aquí tienes la letra de la canción:\n\n*Título:* ${songLyrics.title}\n\n*Letra:*\n${songLyrics.lyrics}`;
+                message.reply(replyMessage);
+            } else {
+                message.reply(
+                    `${robotEmoji} No encontré la letra de esa canción.`
+                );
+            }
+        } catch (error) {
+            console.error(error);
+            message.reply(
+                `${robotEmoji} Hubo un error al buscar la letra de la canción.`
+            );
         }
     }
 }
