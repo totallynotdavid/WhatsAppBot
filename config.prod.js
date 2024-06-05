e477a9afef4ab81170afe776507e61e5d3c8ee48
David Duran <dadch1404@gmail.com>
2024-06-05 11:44:38 -0500

    chore: Update MAX_USER_MSG_LENGTH, MAX_TOKENS, and MAX_CONVERSATION_LENGTH in config.prod.js

--- config.prod.js
+++ config.prod.js
@@ -3,7 +3,7 @@ module.exports = {
     admin_command_prefix: `@`,
     robotEmoji: `🤖`,
     // openai config
-    MAX_USER_MSG_LENGTH: 600,
-    MAX_TOKENS: 800,
-    MAX_CONVERSATION_LENGTH: 5000,
+    MAX_USER_MSG_LENGTH: 800,
+    MAX_TOKENS: 1200,
+    MAX_CONVERSATION_LENGTH: 10000,
 };
