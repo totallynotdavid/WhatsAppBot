04d4b89ec59580d92f32b99c4e78cba57a92fb79
David Duran <dadch1404@gmail.com>
2024-09-10 10:49:33 -0500

    refactor: Update lyrics.js to use proxy wrapper for fetching lyrics

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,5 +1,5 @@
 const { getLyrics } = require(`fetch-lyrics`);
-const { withProxy } = require(`@totallynodavid/proxy-wrapper`);
+const withProxy = require(`@totallynodavid/proxy-wrapper`);
 
 async function handleSongLyricsRequest(stringifyMessage, message, robotEmoji) {
     if (stringifyMessage.length === 1) {
@@ -9,7 +9,8 @@ async function handleSongLyricsRequest(stringifyMessage, message, robotEmoji) {
     } else {
         const songName = stringifyMessage.slice(1).join(` `);
         try {
-            const songLyrics = await withProxy(getLyrics(songName));
+            const getLyricsWithProxy = withProxy(getLyrics);
+            const songLyrics = await getLyricsWithProxy(songName);
             if (songLyrics && songLyrics.lyrics) {
                 const replyMessage = `${robotEmoji} Aquí tienes la letra de la canción:\n\n*Título:* ${songLyrics.title}\n\n*Letra:*\n${songLyrics.lyrics}`;
                 message.reply(replyMessage);
